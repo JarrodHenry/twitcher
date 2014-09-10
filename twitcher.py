44818eb8f45389819a294e736dde415df004b418
Jarrod Henry <jarrodhenry@gmail.com>
2014-09-10 13:39:51 -0500

    Adding in livestreamer dependencies.

--- twitcher.py
+++ twitcher.py
@@ -17,9 +17,16 @@ def main():
                 if stream['channel']['name'] == streamer:
                     active.append(stream)
 
+    count = 0
     for result in active:
-        print("{0}: {1}".format(result['channel']['name'],
-                                result['channel']['url']))
+        print("{0}-{1}: {2}".format(count,
+                                    result['channel']['name'],
+                                    result['channel']['url']))
+        count = count + 1
+
+    choice = input("Please select a stream: ")
+    print(choice)
+    
 
 if __name__ == '__main__':
     main()
