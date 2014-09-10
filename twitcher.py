dff57b2557f7252e50fd2da23cd8294a41ef6c4e
Jarrod Henry <jarrodhenry@gmail.com>
2014-09-10 15:02:25 -0500

    Base path complete, can now go from select to view

--- twitcher.py
+++ twitcher.py
@@ -1,6 +1,7 @@
 from twitch import TwitchTV
 from logging import Logger
-
+import shlex
+import subprocess
 
 streamers = ['joindotared', 'imaqtpie', 'riotgames']
 log = Logger('twitch')
@@ -25,8 +26,9 @@ def main():
         count = count + 1
 
     choice = input("Please select a stream: ")
-    print(choice)
-    
+    url = active[int(choice)]['channel']['url']
+    command = "livestreamer --player mpv {0} high".format(url)
+    subprocess.call(shlex.split(command))
 
 if __name__ == '__main__':
     main()
