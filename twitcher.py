5fda5e12bad002485cd7504c2cb785b10569832d
Jarrod Henry <jarrodhenry@gmail.com>
2014-09-10 10:35:04 -0500

    Now printing online streamers from list, but with errors.

--- twitcher.py
+++ twitcher.py
@@ -1,6 +1,6 @@
 from twitch import TwitchTV
 from logging import Logger
-from pprint import pprint
+
 
 streamers = ['JoinDotaRed', 'RiotGames', 'imaqtpie']
 log = Logger('twitch')
@@ -9,12 +9,17 @@ twitch = TwitchTV(log)
 
 def main():
     active = []
+    count = 0
     for streamer in streamers:
         x = twitch.searchStreams(streamer)
         if x:
-            active.append(x[0])
+            if x[count]['channel']['name'] is streamer:
+                active.append(x[count])
+                count = count + 1
 
-    pprint(x)
+    for result in x:
+        print("{0}: {1}".format(result['channel']['name'],
+                                result['channel']['url']))
 
 if __name__ == '__main__':
     main()
